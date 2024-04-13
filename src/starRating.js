ae176ff5da9dd2b6d3d3ff00db2edd1e46540920
lei <littlesomerest@gmail.com>
2024-04-13 09:17:54 +0800

    prop type

--- src/starRating.js
+++ src/starRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,18 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+// NOT recommended
+// USE TYPESCRIPT to define prop type
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
