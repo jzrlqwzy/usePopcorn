1f6be31d86147da87d83fe1e11fa7e290a3483a8
lei <littlesomerest@gmail.com>
2024-04-15 06:57:02 +0800

    use useRef to focus cursor on search bar

--- src/App.js
+++ src/App.js
@@ -228,9 +228,23 @@ function NumResults({ movies }) {
 function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [setQuery]
+  );
 
   return (
     <input
