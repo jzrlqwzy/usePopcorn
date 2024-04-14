30448ec7ee5cb53e0173e5caca7156ac2ec892b1
lei <littlesomerest@gmail.com>
2024-04-14 15:25:13 +0800

    demonstrate Hook Rule 1

--- src/App.js
+++ src/App.js
@@ -311,6 +311,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     Genre: genre,
   } = movie;
 
+  // Demonstrate Hook Rule 1
+  // if (imdbRating > 8) [isTop, setIsTop] = useState(true);
+  // if (imdbRating > 8) return <p>Greatest ever!</p>;
+
   function handleAdd() {
     const newWatchedMovie = {
       imdbId: selectedId,
