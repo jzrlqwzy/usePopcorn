f58099c87099c2a07aa2e1585fb3bb6a2e9bb399
lei <littlesomerest@gmail.com>
2024-04-19 20:24:19 +0800

    add dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import React, { useState } from "react";
 // import StarRating from "./starRating";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
